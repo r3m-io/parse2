9bc1ee563de08299a47a8dac25c4fa5b2d787262
remco van der Velde <remco.pc@outlook.com>
2024-07-31 15:24:50 +0200

    init parse

---
+++ src/Package/Service/Parse.php
@@ -0,0 +1,40 @@
+<?php
+namespace Package\R3m\Io\Task\Service;
+
+use R3m\Io\App;
+
+use R3m\Io\Module\File;
+
+use Exception;
+class Parse
+{
+
+
+    /**
+     * @throws Exception
+     */
+    public static function compile(App $object, $flags, $options){
+        if(!property_exists($options, 'source')){
+            throw new Exception('Source not found');
+        }
+        if(File::exist($options->source) === false){
+            throw new Exception('Source not found');
+        }
+        // Step 1: Read the template file
+        $template = File::read($options->source);
+
+        // Step 2: Define the placeholder values
+        $placeholders = [
+                'name' => 'John Doe',
+                'age' => '30',
+                // Add more placeholders and their replacements as needed
+        ];
+
+        // Step 3: Replace placeholders with actual values
+        foreach ($placeholders as $key => $value) {
+            $template = str_replace('{{' . $key . '}}', $value, $template);
+        }
+        // Step 4: Output the processed template
+        dd($template);
+    }
+}
--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -4,33 +4,19 @@ namespace Package\R3m\Io\Parse\Trait;
 use R3m\Io\App;
 use R3m\Io\Config;
 
-use R3m\Io\Module\Cli;
-use R3m\Io\Module\Core;
-
-use R3m\Io\Node\Model\Node;
-
-
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7;
-
-use GuzzleHttp\Exception\RequestException;
-use GuzzleHttp\Exception\GuzzleException;
+use Package\R3m\Io\Parse\Service\Parse;
 
 use Exception;
-
-use R3m\Io\Exception\FileWriteException;
-use R3m\Io\Exception\ObjectException;
 
 trait Main {
 
     /**
-     * @throws ObjectException
-     * @throws FileWriteException
      * @throws Exception
      */
     public function compile($flags, $options){
+        $object = $this->object();
+        Parse::compile($object, $flags, $options);
         d($flags);
         ddd($options);
-        $object = $this->object();
     }
 }
