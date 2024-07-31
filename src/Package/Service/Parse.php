f09bedf6340107c5d7b05aaa847f1cd0056cba05
remco van der Velde <remco.pc@outlook.com>
2024-07-31 20:43:33 +0200

    init parse

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -243,15 +243,58 @@ class Parse
                                 $before .= $char;
                             }
                         }
-                        $record['variable'] = [
-                            'is_assign' => true,
-                            'operator' => $operator,
-                            'name' => substr($before, 1),
-                            'value' => Parse::value($object, [
+                        $value = Parse::value(
+                            $object,
+                            [
                                 'string' => $after,
                                 'array' => $after_array
-                            ], $flags, $options)
-                        ];
+                            ],
+                            $flags,
+                            $options
+                        );
+                        if(array_key_exists('is_array', $value)){
+                            $record['variable'] = [
+                                'is_assign' => true,
+                                'operator' => $operator,
+                                'name' => substr($before, 1),
+                                'execute' => $value['execute'],
+                                'is_array' => true
+                            ];
+                        }
+                        elseif(array_key_exists('is_null', $value)){
+                            $record['variable'] = [
+                                'is_assign' => true,
+                                'operator' => $operator,
+                                'name' => substr($before, 1),
+                                'execute' => $value['execute'],
+                                'is_null' => true
+                            ];
+                        }
+                        elseif(array_key_exists('is_boolean', $value)){
+                            $record['variable'] = [
+                                'is_assign' => true,
+                                'operator' => $operator,
+                                'name' => substr($before, 1),
+                                'execute' => $value['execute'],
+                                'is_boolean' => true
+                            ];
+                        }
+                        elseif(array_key_exists('is_single_quoted', $value)){
+                            $record['variable'] = [
+                                'is_assign' => true,
+                                'operator' => $operator,
+                                'name' => substr($before, 1),
+                                'execute' => $value['execute'],
+                                'is_single_quoted' => true
+                            ];
+                        } else {
+                            $record['variable'] = [
+                                'is_assign' => true,
+                                'operator' => $operator,
+                                'name' => substr($before, 1),
+                                'execute' => $value['execute']
+                            ];
+                        }
                         ddd($record);
                     }
                 }
@@ -266,19 +309,34 @@ class Parse
         $value = $input['string'] ?? null;
         switch($value){
             case '[]':
-                return [];
+                return [
+                    'execute' => [],
+                    'is_array' => true
+                ];
             case 'true':
-                return true;
+                return [
+                    'execute' => true,
+                    'is_boolean' => true
+                ];
             case 'false':
-                return false;
+                return [
+                    'execute' => false,
+                    'is_boolean' => true
+                ];
             case 'null':
-                return null;
+                return [
+                    'execute' => null,
+                    'is_null' => true
+                ];
             default:
                 if(
                     substr($value, 0, 1) === '\'' &&
                     substr($value, -1) === '\''
                 ){
-                    return substr($value, 1, -1);
+                    return [
+                        'execute' => substr($value, 1, -1),
+                        'is_single_quoted' => true
+                    ];
                 }
                 return Parse::value_split($object, $input['array'], $flags, $options);
         }
