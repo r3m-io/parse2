4fd8d9cf238971394840746dff5c0694e1dc25b0
remco van der Velde <remco.pc@outlook.com>
2024-07-31 16:24:03 +0200

    init parse

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -22,9 +22,9 @@ class Parse
         // Step 1: Read the template file
         $template = File::read($options->source);
 
-        $words = Parse::words($object, $template, $flags, $options);
+        $token = Parse::token($object, $template, $flags, $options);
 
-        ddd($words);
+        ddd($token);
 
         // Step 2: Define the placeholder values
         $placeholders = [
@@ -40,28 +40,8 @@ class Parse
         dd($template);
     }
 
-    public static function words(App $object, $string=''){
-        // Check for matches
-        $pattern = '/{{[^}]*}}|\S+|\s+/';
-        $result = [];
-        if (preg_match_all($pattern, $string, $matches, PREG_OFFSET_CAPTURE)) {
-            foreach ($matches[0] as $match) {
-                $matchText = $match[0];
-                $startPos = $match[1];
-                $lineNumber = substr_count(substr($string, 0, $startPos), "\n") + 1;
-                $lineStartPos = strrpos(substr($string, 0, $startPos), "\n") + 1;
-                $charPos = $startPos - $lineStartPos + 1;
-                $length = strlen($matchText);
-                $result[] = [
-                    'match' => $matchText,
-                    'line' => $lineNumber,
-                    'column' => [
-                        'start' => $charPos,
-                        'end' => $charPos + $length
-                    ]
-                ];
-            }
-        }
-        return $result;
+    public static function token(App $object, $string=''){
+        $split = mb_str_split($string, 1);
+        ddd($split);
     }
 }
