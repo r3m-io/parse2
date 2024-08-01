6be1bcea950db5ea47033f5d0f6f05dd87cab36a
remco van der Velde <remco.pc@outlook.com>
2024-08-01 13:02:54 +0200

    init parse

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -430,6 +430,7 @@ class Parse
     public static function operator_solve(App $object, $input, $flags, $options){
 
         $input = Parse::operator_define($object, $input, $flags, $options);
+        d($input);
         while(Parse::operator_has($object, $input, $flags, $options)){
 //            $statement = Parse::remove_whitespace($object, $input, $flags, $options);
             $operator = Parse::operator_get($object, $input, $flags, $options);
