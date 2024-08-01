33fa6ffd5bdd5f3f6fc77c412251d2778bbe7b68
remco van der Velde <remco.pc@outlook.com>
2024-08-01 17:55:07 +0200

    init parse

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -648,6 +648,8 @@ class Parse
             case '=':
             case '==':
             case '===':
+            case '?':
+            case '??':
             case '.':
             case '.=':
             case '+=':
