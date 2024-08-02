cfc1d2a68e2fb113ab3e70ed01f86d331a4c7123
remco van der Velde <remco.pc@outlook.com>
2024-08-02 19:39:44 +0200

    parse

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -1233,6 +1233,7 @@ class Parse
         $counter = 0;
         $input = Symbol::define($object, $input, $flags, $options);
         $input = Cast::define($object, $input, $flags, $options);
+        d($input);
         $input = Method::define($object, $input, $flags, $options);
         $input = Variable::define($object, $input, $flags, $options);
         $input = Parse::remove_whitespace($object, $input, $flags, $options);
