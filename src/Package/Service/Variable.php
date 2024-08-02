7f801eb69ebcea84e47c6081a92261e645d6be2c
remco van der Velde <remco.pc@outlook.com>
2024-08-02 18:19:13 +0200

    parse

--- src/Package/Service/Variable.php
+++ src/Package/Service/Variable.php
@@ -10,6 +10,68 @@ use Exception;
 class Variable
 {
     public static function define(App $object, $input, $flags, $options){
+        $count = count($input);
+        $is_variable = false;
+        foreach($input as $nr => $char){
+            $previous = $input[$nr - 1] ?? null;
+            if(is_array($char) && array_key_exists('value', $char)){
+                if($char['value'] === '$'){
+                    $is_variable = $nr;
+                    $name = '';
+                    for($i = $nr + 1; $i < $count; $i++){
+                        if(
+                            is_array($input[$i]) &&
+                            array_key_exists('value', $input[$i])
+                        ){
+                            if(
+                                in_array(
+                                    $input[$i]['value'],
+                                    [
+                                        '_',
+                                        '.'
+                                    ]
+                                )
+                            ){
+                                $name .= $input[$i]['value'];
+                            } else {
+                                break;
+                            }
+                        } else {
+                            $name .= $char;
+                        }
+                    }
+                    if($name){
+                        $input[$is_variable] = [
+                            'type' => 'variable',
+                            'value' => $name
+                        ];
+                        for($i = $is_variable + 1; $i < $count; $i++){
+                            if(
+                                is_array($input[$i]) &&
+                                array_key_exists('value', $input[$i])
+                            ){
+                                if(
+                                    in_array(
+                                        $input[$i]['value'],
+                                        [
+                                            '_',
+                                            '.'
+                                        ]
+                                    )
+                                ){
+                                    $input[$i] = null;
+                                } else {
+                                    break;
+                                }
+                            } else {
+                                $input[$i] = null;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
         d($input);
 
         return $input;
