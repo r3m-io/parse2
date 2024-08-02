794d58e29f1bb751a6f723cb3e05e1f2f3364644
remco van der Velde <remco.pc@outlook.com>
2024-08-02 11:29:45 +0200

    init parse

--- src/Package/Service/Cast.php
+++ src/Package/Service/Cast.php
@@ -18,7 +18,7 @@ class Cast
                 array_key_exists('value', $char) &&
                 $char['value'] === '('
             ){
-                $is_collect = true;
+                $is_collect = $nr;
             }
             elseif(
                 is_array($char) &&
@@ -26,12 +26,70 @@ class Cast
                 $char['value'] === ')'
             ){
                 if(strlen($define) > 0){
-                    ddd($define);
+                    $is_define = false;
+                    switch(strtolower($define)){
+                        case 'int':
+                        case 'integer':
+                            $input[$is_collect] = [
+                                'value' => $define,
+                                'is_cast' => true,
+                                'cast' => 'integer'
+                            ];
+                            $is_define = true;
+                        break;
+                        case 'float':
+                        case 'double':
+                            $input[$is_collect] = [
+                                'value' => $define,
+                                'is_cast' => true,
+                                'cast' => 'float'
+                            ];
+                            $is_define = true;
+                        break;
+                        case 'boolean':
+                        case 'bool':
+                            $input[$is_collect] = [
+                                'value' => $define,
+                                'is_cast' => true,
+                                'cast' => 'boolean'
+                            ];
+                            $is_define = true;
+                        break;
+                        case 'array':
+                            $input[$is_collect] = [
+                                'value' => $define,
+                                'is_cast' => true,
+                                'cast' => 'array'
+                            ];
+                            $is_define = true;
+                        break;
+                        case 'object':
+                            $input[$is_collect] = [
+                                'value' => $define,
+                                'is_cast' => true,
+                                'cast' => 'object'
+                            ];
+                            $is_define = true;
+                        break;
+                        case 'clone':
+                            $input[$is_collect] = [
+                                'value' => $define,
+                                'is_cast' => true,
+                                'cast' => 'clone'
+                            ];
+                            $is_define = true;
+                        break;
+                    }
+                    if($is_define){
+                        for($i = $is_collect + 1; $i <= $nr; $i++){
+                            $input[$i] = null;
+                        }
+                    }
                 }
                 $is_collect = false;
             }
             elseif(
-                $is_collect &&
+                $is_collect !== false &&
                 !is_array($char)
             ){
                 if(
