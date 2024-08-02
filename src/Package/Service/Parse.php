aa0d4da1ea32e0a20635b25618c6e526081acfcc
remco van der Velde <remco.pc@outlook.com>
2024-08-02 21:53:47 +0200

    parse

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -498,7 +498,7 @@ class Parse
                             $argument_array = [];
                         }
                         if($modifier_name){
-                            ddd($argument_list);
+                            d($argument_list);
                             $modifier_list[] = [
                                 'name' => $modifier_name,
                                 'argument' => $argument_list
