1f99abaf6c35a361991ad145e22ccf992f10501b
remco van der Velde <remco.pc@outlook.com>
2024-08-03 21:44:06 +0200

    parse

--- src/Package/Service/Parse.php
+++ src/Package/Service/Parse.php
@@ -1400,7 +1400,8 @@ class Parse
         $list = [];
 
         $counter = 0;
-        ddd($input);
+        d($input);
+        trace();
         $input = Symbol::define($object, $input, $flags, $options);
         $input = Cast::define($object, $input, $flags, $options);
         $input = Method::define($object, $input, $flags, $options);
